9e42553ec8df4c6fec698c41686ee16336bf248d
bzh <bzh@pop-os.localdomain>
2023-11-01 22:25:29 +0200

    Beginnig to setup Spring Config

---
+++ mobiLeLeLe-project/src/main/java/bg/softuni/mobiLeLeLe/config/SecurityConfiguration.java
@@ -0,0 +1,15 @@
+package bg.softuni.mobiLeLeLe.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfiguration {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) {
+        httpSecurity
+                .
+    }
+}
